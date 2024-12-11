6e8b2fd1550cec52a94204b980eabe83773b973d
elittech <152604078+hachizeus@users.noreply.github.com>
2024-12-11 10:19:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/patt-rentals/', // Replace with your repository name
+  base: '/pattrentals.org/', // Replace with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
